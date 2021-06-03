f2a935082c8f0e3e38fe871871e779b5d176c3d0
Jakraes <70046168+Jakraes@users.noreply.github.com>
2021-06-03 06:23:31 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ for y in range(len(world)):
             Entity(model="cube", texture="wall.png", collider="box", position = (x*3, 0, y*3), scale = 3)
         elif world[y][x] == "_":
             Entity(model="plane", scale=3, texture="floor.png", collider="mesh", position = (x*3, -3/2, y*3))
-            Entity(model="plane", scale=3, texture="ceil.png", collider="mesh", position=(x * 3, 3/2, y * 3), rotation = (180, 0, 0))
+            Entity(model="plane", scale=3, texture="ceil.png", position=(x * 3, 3/2, y * 3), rotation = (180, 0, 0))
 
 floor_tiles = []
 for y in range(len(world)):
@@ -23,7 +23,7 @@ for y in range(len(world)):
             floor_tiles.append([y, x])
 
 spawn = floor_tiles[random.randint(0, len(floor_tiles)-1)]
-player.y = -0.7
+player.y = 1
 player.x = spawn[1]*3
 player.z = spawn[0]*3
 
